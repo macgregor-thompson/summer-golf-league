f2f251a53cf65853b365969ff6d62e2a0d316040
MacGregor Thompson <mhthompson86@gmail.com>
2018-03-10 14:48:50 -0500

    add prod environment firebase variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDR5SEEhFjBZIxqMXoU-a8xoZujeY3Q5Qc',
+    authDomain: 'summer-golf-league.firebaseapp.com',
+    databaseURL: 'https://summer-golf-league.firebaseio.com',
+    projectId: 'summer-golf-league',
+    storageBucket: '',
+    messagingSenderId: '1026158852457'
+  }
 };
