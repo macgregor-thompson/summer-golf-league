1e96d1cbe89680706e85075cf56572fe7a7084b0
MacGregor Thompson <mhthompson86@gmail.com>
2018-03-10 14:55:28 -0500

    environment variable bullshit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 // AngularFire
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
-import { environment } from '../environments/environment';
+import { environment } from '../environments/environment.prod';
 
 
 // Angular Material
