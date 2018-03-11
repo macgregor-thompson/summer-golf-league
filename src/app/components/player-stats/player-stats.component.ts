eb34aa9e019fda58cc1203703df823a21b4f4560
MacGregor Thompson <mhthompson86@gmail.com>
2018-03-11 11:27:53 -0400

    add some new charts and a charttype enum

--- src/app/components/player-stats/player-stats.component.ts
+++ src/app/components/player-stats/player-stats.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, AfterViewInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material';
 
 import { MockDataService } from '../../services/mock-data.service';
 import { Golfer } from '../../models/golfer';
 import { Round } from '../../models/round';
 import { Score } from '../../models/score';
+import { ChartTypes } from '../../models/enums/chart-types.enum';
 
 
 @Component({
@@ -19,12 +20,28 @@ export class PlayerStatsComponent implements OnInit {
 
   spinner = false;
   scores: object[];
-  birdies = 0;
-  pars = 0;
-  bogies = 0;
-  doubles = 0;
-  eagles = 0;
-  others = 0;
+  showingNetScores: true;
+  gross = {
+    scores: [],
+    birdies: 0,
+    pars: 0,
+    bogies: 0,
+    doubles: 0,
+    eagles: 0,
+    others: 0
+  };
+  net = {
+    scores: [],
+    birdies: 0,
+    pars: 0,
+    bogies: 0,
+    doubles: 0,
+    eagles: 0,
+    others: 0
+  };
+  ChartTypes = ChartTypes;
+  chartType = ChartTypes.Pie;
+  view = [700, 400];
   colorScheme = {
     domain: [
       '#f44336', // birdies
@@ -58,66 +75,131 @@ export class PlayerStatsComponent implements OnInit {
 
   analyzeScores() {
     this.spinner = true;
-    this.rounds.forEach((round: Round) => {
-      round.scores.map((score: Score) => {
-        if (score.score === score.par) {
-          this.pars++;
-        } else if (score.score === score.par + 1) {
-          this.bogies++;
-        } else if (score.score === score.par + 2) {
-          this.doubles++;
-        } else if (score.score === score.par - 1) {
-          this.birdies++;
-        } else if (score.score === score.par - 2) {
-          this.eagles++;
-        } else {
-          this.others++;
-        }
-      });
-    });
-    this.scores = [
-      { name: 'Birdies', value: this.birdies },
-      { name: 'Pars', value: this.pars },
-      { name: 'Bogies', value: this.bogies },
-      { name: 'Doubles', value: this.doubles },
-      { name: 'Eagles', value: this.eagles },
-      { name: 'Others', value: this.others }
-    ];
+    this.analyzeGrossScores();
+    this.analyzeNetScores();
+    this.scores = this.net.scores;
+
     this.spinner = false;
   }
 
- /* calculateAverage() {
-    let totals = 0;
+  analyzeNetScores() {
     this.rounds.forEach((round: Round) => {
-      totals = totals + round.total;
+      round.scores.map((score: Score) => {
+        const netScore = score.handicap <= round.courseHandicap * 2 ? score.score - 1 : score.score;
+        if (netScore === score.par) {
+          this.net.pars++;
+        } else if (netScore === score.par + 1) {
+          this.net.bogies++;
+        } else if (netScore === score.par + 2) {
+          this.net.doubles++;
+        } else if (netScore === score.par - 1) {
+          this.net.birdies++;
+        } else if (netScore === score.par - 2) {
+          this.net.eagles++;
+        } else {
+          this.net.others++;
+        }
+      });
     });
-    this.passUpAverage.emit(totals / this.rounds.length);
-  }*/
+
+    this.net.scores = [
+      { name: 'Birdies', value: this.net.birdies },
+      { name: 'Pars', value: this.net.pars },
+      { name: 'Bogies', value: this.net.bogies },
+      { name: 'Doubles', value: this.net.doubles },
+      { name: 'Eagles', value: this.net.eagles },
+      { name: 'Others', value: this.net.others }
+    ];
+  }
+
+
+  analyzeGrossScores() {
+    this.rounds.forEach((round: Round) => {
+      round.scores.map((score: Score) => {
+        if (score.score === score.par) {
+          this.gross.pars++;
+        } else if (score.score === score.par + 1) {
+          this.gross.bogies++;
+        } else if (score.score === score.par + 2) {
+          this.gross.doubles++;
+        } else if (score.score === score.par - 1) {
+          this.gross.birdies++;
+        } else if (score.score === score.par - 2) {
+          this.gross.eagles++;
+        } else {
+          this.gross.others++;
+        }
+      });
+    });
+
+    this.gross.scores = [
+      { name: 'Birdies', value: this.gross.birdies },
+      { name: 'Pars', value: this.gross.pars },
+      { name: 'Bogies', value: this.gross.bogies },
+      { name: 'Doubles', value: this.gross.doubles },
+      { name: 'Eagles', value: this.gross.eagles },
+      { name: 'Others', value: this.gross.others }
+    ];
+  }
+
+  /* calculateAverage() {
+     let totals = 0;
+     this.rounds.forEach((round: Round) => {
+       totals = totals + round.total;
+     });
+     this.passUpAverage.emit(totals / this.rounds.length);
+   }*/
 
   onSelect(event) {
     console.log('event:', event);
-    switch (event.name) {
-      case 'Doubles':
-        if (this.doubles > this.bogies) {
-          this.openSnackBar('You have more doubles than bogies! You suck!!!', 'YOU SUCK!!!');
-        }
-        break;
-      case 'Others':
-        if (this.others > this.pars) {
-          this.openSnackBar('You have more others than pars...You should just give up golf!', 'YOU SUCK!!!');
-        }
-        break;
-      case 'Birdies':
-        if (this.rounds.length > this.birdies) {
-          this.openSnackBar('You have more rounds than birdies...', 'HAHA!');
-        }
-        break;
-      case 'Pars':
-        if (this.pars > this.birdies) {
-          this.openSnackBar('Will you make some birdies for once???', 'YA BITCH!!!');
-        }
-        break;
+    if (this.showingNetScores) {
+      switch (event.name) {
+        case 'Doubles':
+          if (this.net.doubles > this.net.bogies) {
+            this.openSnackBar('You have more doubles than bogies! You suck!!!', 'YOU SUCK!!!');
+          }
+          break;
+        case 'Others':
+          if (this.net.others > this.net.pars) {
+            this.openSnackBar('You have more others than pars...You should just give up golf!', 'YOU SUCK!!!');
+          }
+          break;
+        case 'Birdies':
+          if (this.rounds.length > this.net.birdies) {
+            this.openSnackBar('You have more rounds than birdies...', 'HAHA!');
+          }
+          break;
+        case 'Pars':
+          if (this.net.pars > this.net.birdies) {
+            this.openSnackBar('Will you make some birdies for once???', 'YA BITCH!!!');
+          }
+          break;
+      }
+    } else {
+      switch (event.name) {
+        case 'Doubles':
+          if (this.gross.doubles > this.gross.bogies) {
+            this.openSnackBar('You have more doubles than bogies! You suck!!!', 'YOU SUCK!!!');
+          }
+          break;
+        case 'Others':
+          if (this.gross.others > this.gross.pars) {
+            this.openSnackBar('You have more others than pars...You should just give up golf!', 'YOU SUCK!!!');
+          }
+          break;
+        case 'Birdies':
+          if (this.rounds.length > this.gross.birdies) {
+            this.openSnackBar('You have more rounds than birdies...', 'HAHA!');
+          }
+          break;
+        case 'Pars':
+          if (this.gross.pars > this.gross.birdies) {
+            this.openSnackBar('Will you make some birdies for once???', 'YA BITCH!!!');
+          }
+          break;
+      }
     }
+
   }
 
   openSnackBar(message: string, action: string) {
--- src/app/models/round.ts
+++ src/app/models/round.ts
@@ -1,11 +1,13 @@
 import { SidePlayed } from './enums/side-played.enum';
 import { Score } from './score';
+import { Team } from './team';
 
 export interface Round {
   id: number;
   individual: boolean;
   golfers: number[];
-  golferId: number; //TODO: refactor to just use the golfers array
+  // TODO: refactor to just use the golfers array or do we just check if individual round or not and then check the array
+  golferId: number;
   date: string;
   week: number;
   courseId: number;
@@ -14,6 +16,7 @@ export interface Round {
   total: number;
   par: number;
   handicap: number; // remember this value will change every few weeks...
-  courseHandicap: number;
+  courseHandicap: number; // value will change so we need to have a handicap "tagged" with a round
+  team: Team;
   scores: Score[];
 }
