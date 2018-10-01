31444e0274aa56e238926e2134e6cd2784bc4ba4
MacGregor Thompson <mhthompson86@gmail.com>
2018-10-01 15:23:12 -0400

    add back in the ngx-charts for showing the team points in a few different ways.

--- src/app/rankings/rankings-dashboard/rankings-dashboard.component.ts
+++ src/app/rankings/rankings-dashboard/rankings-dashboard.component.ts
@@ -14,29 +14,11 @@ import { IGolfer } from '../../models/interfaces/i-golfer';
 })
 export class RankingsDashboardComponent implements OnInit {
   spinner = false;
-  points: NgxObject[] = [];
   teams: Team[];
   teamData: MatTableDataSource<Team>;
   golfers: MatTableDataSource<IGolfer>;
   members: IGolfer[];
-  ChartTypes = ChartTypes;
-  chartType = ChartTypes.NumberCard;
-  // bar chart options
-  showXAxis = true;
-  showYAxis = true;
-  gradient = false;
-  showLegend = true;
-  showXAxisLabel = true;
-  xAxisLabel = 'Teams';
-  showYAxisLabel = true;
-  yAxisLabel = 'Points';
-  colorScheme = {
-    domain: [
-      //'#f44336', // MacGregor's team
-      // '#43a047', // Warbird's team
-      // '#2196f3', // GanMan's team
-    ]
-  };
+
   teamColumns = ['teamRanking', 'teamName', 'teamNetPoints', 'teamPoints', 'teamWorstWeek', 'teamSecondWorstWeek', 'teamBonusPoints', 'teamPointsBehind'];
   playerColumns = ['ranking', 'displayName', 'netPoints', 'points', 'worstWeek', 'secondWorstWeek', 'pointsBehind', 'team'];
   playerFirstPoints = 0;
@@ -68,17 +50,7 @@ export class RankingsDashboardComponent implements OnInit {
         this.teamData = new MatTableDataSource<Team>(rankedTeams);
       }, e => console.log('Error fetching teams:', e));
 
-    // This is for the ngx-charts
-    /* this.afs.collection<Team>('teams', ref => ref.where('id', '<', 4)).valueChanges()
-       .subscribe((data: Team[]) => {
-         this.teams = data;
-         data.forEach((team: Team) => {
-           this.points.push({
-             name: team.name,
-             value: team.points
-           });
-         });
-       }, e => console.log('Error fetching teams:', e));*/
+
 
     this.afs.collection<IGolfer>('members', ref => ref.orderBy('netPoints', 'desc')).valueChanges()
       .subscribe((data: IGolfer[]) => {
@@ -125,30 +97,5 @@ export class RankingsDashboardComponent implements OnInit {
     }
   }
 
-  onSelect(event) {
-    console.log('event:', event, this.points);
 
-    /*   switch (event.name) {
-         case '':
-           if (this.gross.doubles > this.gross.bogies) {
-             this.openSnackBar('You have more doubles than bogies! You suck!!!', 'YOU SUCK!!!');
-           }
-           break;
-         case 'Others':
-           if (this.gross.others > this.gross.pars) {
-             this.openSnackBar('You have more others than pars...You should just give up golf!', 'YOU SUCK!!!');
-           }
-           break;
-         case 'Birdies':
-           if (this.rounds.length > this.gross.birdies) {
-             this.openSnackBar('You have more rounds than birdies...', 'HAHA!');
-           }
-           break;
-         case 'Pars':
-           if (this.gross.pars > this.gross.birdies) {
-             this.openSnackBar('Will you make some birdies for once???', 'YA BITCH!!!');
-           }
-           break;
-       }*/
-  }
 }
--- src/app/stats/stats-dashboard/stats-dashboard.component.ts
+++ src/app/stats/stats-dashboard/stats-dashboard.component.ts
@@ -6,6 +6,9 @@ import { Team } from '../../models/interfaces/team';
 import { IMatch } from '../../models/interfaces/i-match';
 import { DataService } from '../../core/services/data.service';
 import { IWeek } from '../../models/interfaces/i-week';
+import { ChartTypes } from '../../models/enums/chart-types.enum';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { NgxObject } from '../../models/interfaces/ngx-object';
 
 
 @Component({
@@ -17,11 +20,30 @@ export class StatsDashboardComponent implements OnInit {
   golfers: IGolfer[];
   membersByLeaguePoints: IGolfer[];
   teamsByLeaguePoints: Team[];
+  points: NgxObject[] = [];
   matches: IMatch[];
   weeks: IWeek[];
   completedWeeks: IWeek[];
   teams: Team[];
   step = -1;
+  ChartTypes = ChartTypes;
+  chartType = ChartTypes.NumberCard;
+  // bar chart options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Teams';
+  showYAxisLabel = true;
+  yAxisLabel = 'Points';
+  colorScheme = {
+    domain: [
+      //'#f44336', // MacGregor's team
+      //'#43a047', // Warbird's team
+      //'#2196f3', // GanMan's team
+    ]
+  };
 
   constructor(private ds: DataService,
               public dialog: MatDialog) {}
@@ -30,13 +52,25 @@ export class StatsDashboardComponent implements OnInit {
     this.ds.members().subscribe((data: IGolfer[]) => this.golfers = data);
     this.ds.membersOrderedBy('netPoints', 'desc').subscribe((data: IGolfer[]) => this.membersByLeaguePoints = data);
     this.ds.teams().subscribe((data: Team[]) => this.teams = data);
-    this.ds.teamsOrderedBy('netPoints', 'desc').subscribe((data: Team[]) => this.teamsByLeaguePoints = data);
+    this.ds.teamsOrderedBy('netPoints', 'desc').subscribe((data: Team[]) => {
+      this.teamsByLeaguePoints = data;
+      // This is for the ngx-charts
+      data.forEach((team: Team, i) => {
+        this.points.push({
+          name: `Team ${team.name}`,
+          value: team.points
+        });
+        this.colorScheme.domain[i] = team.colorScheme;
+      });
+    });
     this.ds.matches().subscribe((data: IMatch[]) => this.matches = data);
     this.ds.weeks().subscribe((data: IWeek[]) => {
       this.weeks = data;
       this.completedWeeks = data.filter(w => w.completed);
     });
   }
+
+
 
 
   worstWeek(weeklyPoints) {
@@ -77,6 +111,33 @@ export class StatsDashboardComponent implements OnInit {
 
   prevStep() {
     this.step--;
+  }
+
+  onSelect(event) {
+    console.log('event:', event, this.points);
+
+    /*   switch (event.name) {
+         case '':
+           if (this.gross.doubles > this.gross.bogies) {
+             this.openSnackBar('You have more doubles than bogies! You suck!!!', 'YOU SUCK!!!');
+           }
+           break;
+         case 'Others':
+           if (this.gross.others > this.gross.pars) {
+             this.openSnackBar('You have more others than pars...You should just give up golf!', 'YOU SUCK!!!');
+           }
+           break;
+         case 'Birdies':
+           if (this.rounds.length > this.gross.birdies) {
+             this.openSnackBar('You have more rounds than birdies...', 'HAHA!');
+           }
+           break;
+         case 'Pars':
+           if (this.gross.pars > this.gross.birdies) {
+             this.openSnackBar('Will you make some birdies for once???', 'YA BITCH!!!');
+           }
+           break;
+       }*/
   }
 
 }
